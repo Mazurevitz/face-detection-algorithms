57054dbaa46d0d281c35b000dc9647a0cd6f62c9
Wojciech Mazur <wojtekmaz.glowny@gmail.com>
2018-10-28 23:24:50 +0100

    Haar folder, drawing rectangle, new ignores

--- facesrec.py
+++ facesrec.py
@@ -3,9 +3,24 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
+
 while(True):
     #capture frame by frame
     ret, frame = cap.read()
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(grey, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x,y,w,h)
+        roi_grey = grey[y:y+h, x:x+w]
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_grey)
+
+        color = (255, 0, 0)
+        stroke = 2
+        end_coord_x = x + w
+        end_coord_y = y + h
+        cv2.rectangle(frame, (x,y), (end_coord_x, end_coord_y), color, stroke)
 
     #display resulting frame
     cv2.imshow('frame', frame)
@@ -14,4 +29,3 @@ while(True):
 
 cap.release()
 cv2.destroyAllWindows()
-
