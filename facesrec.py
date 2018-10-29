eafadd25611bcfde3ebccf95f6a0a05de89a6bc1
Wojciech Mazur <wojciech.mazur@gorrion.pl>
2018-10-29 01:50:23 +0100

    added photos, trainer.yml and updated trainer, added labeling

--- facesrec.py
+++ facesrec.py
@@ -1,9 +1,17 @@
 import numpy as np
 import cv2
+import pickle
 
 cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainer.yml")
+
+labels = {}
+with open("labels.pickle", 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
 
 while(True):
     #capture frame by frame
@@ -11,10 +19,22 @@ while(True):
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grey, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
-        print(x,y,w,h)
+        # print(x,y,w,h)
         roi_grey = grey[y:y+h, x:x+w]
-        img_item = "my-image.png"
-        cv2.imwrite(img_item, roi_grey)
+        roi_color = frame[y:y+h, x:x+w]
+
+        id_, conf = recognizer.predict(roi_grey)
+        if conf>=65 and conf <= 90:
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            color = (255,255,255)
+            stroke = 2
+            name = labels[id_]
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+        img_item = "my-image-clr.png"
+        cv2.imwrite(img_item, roi_color)
 
         color = (255, 0, 0)
         stroke = 2
