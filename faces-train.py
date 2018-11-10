ef0e9fa1dcc9c1c4f4932d6d7fac66aec028d4af
Wojciech Mazur <wojtekmaz.glowny@gmail.com>
2018-11-10 19:05:13 +0100

    Updated main() structure

--- faces-train.py
+++ faces-train.py
@@ -1,50 +1,53 @@
 import os
+import pickle
 from PIL import Image
 import numpy as np
 import cv2
-import pickle
 
-face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
-recognizer = cv2.face.LBPHFaceRecognizer_create()
+def main():
+    face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
+    recognizer = cv2.face.EigenFaceRecognizer_create()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR, "images")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    image_dir = os.path.join(BASE_DIR, "images")
 
-x_train = []
-y_labels = []
-label_ids = {}
-current_id = 0
+    x_train = []
+    y_labels = []
+    label_ids = {}
+    current_id = 0
 
-for root, dirs, files in os.walk(image_dir):
-    for file in files:
-        if file.endswith("png") or file.endswith("jpg"):
-            path = os.path.join(root, file)
-            label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
-            # print(label, path)
-            if not label in label_ids:
-                label_ids[label] = current_id
-                current_id += 1
-            id_ = label_ids[label]
-            # print(label_ids)
+    for root, dirs, files in os.walk(image_dir):
+        for file in files:
+            if file.endswith("png") or file.endswith("jpg"):
+                path = os.path.join(root, file)
+                label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
+                # print(label, path)
+                if not label in label_ids:
+                    label_ids[label] = current_id
+                    current_id += 1
+                id_ = label_ids[label]
+                # print(label_ids)
 
-            pil_image = Image.open(path).convert("L") #greyscale
-            size = (300,300)
-            final_image = pil_image.resize(size, Image.ANTIALIAS)
-            
-            image_array = np.array(pil_image, "uint8")
-            # print(image_array)
-            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+                pil_image = Image.open(path).convert("L") #greyscale
+                size = (300, 300)
+                final_image = pil_image.resize(size, Image.ANTIALIAS)
+                
+                image_array = np.array(final_image, "uint8")
+                # print(image_array)
+                faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
-            for (x,y,w,h) in faces:
-                roi = image_array[y:y+h, x:x+w]
-                x_train.append(roi)
-                y_labels.append(id_)
+                for (x, y, w, h) in faces:
+                    roi = image_array[y:y+h, x:x+w]
+                    x_train.append(roi)
+                    y_labels.append(id_)
 
-# print(y_labels)
-# print(x_train)
+    # print(y_labels)
+    # print(x_train)
 
-with open("labels.pickle", 'wb') as f:
-    pickle.dump(label_ids, f)
+    with open("labels.pickle", 'wb') as f:
+        pickle.dump(label_ids, f)
 
-recognizer.train(x_train, np.array(y_labels))
-recognizer.save("trainer.yml")
+    recognizer.train(x_train, np.array(y_labels))
+    recognizer.save("trainer.yml")
+
+main()
--- facesrec.py
+++ facesrec.py
@@ -2,50 +2,53 @@ import numpy as np
 import cv2
 import pickle
 
-cap = cv2.VideoCapture(0)
+def main():
+    cap = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read("trainer.yml")
+    face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
+    recognizer = cv2.face.EigenFaceRecognizer_create()
+    recognizer.read("trainer.yml")
 
-labels = {}
-with open("labels.pickle", 'rb') as f:
-    og_labels = pickle.load(f)
-    labels = {v:k for k,v in og_labels.items()}
+    labels = {}
+    with open("labels.pickle", 'rb') as f:
+        og_labels = pickle.load(f)
+        labels = {v:k for k,v in og_labels.items()}
 
-while(True):
-    #capture frame by frame
-    ret, frame = cap.read()
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(grey, scaleFactor=1.5, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        # print(x,y,w,h)
-        roi_grey = grey[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+    while(True):
+        #capture frame by frame
+        ret, frame = cap.read()
+        grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(grey, scaleFactor=1.5, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            # print(x,y,w,h)
+            roi_grey = grey[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
 
-        id_, conf = recognizer.predict(roi_grey)
-        if conf>=65 and conf <= 90:
-            print(id_)
-            print(labels[id_])
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            color = (255,255,255)
+            id_, conf = recognizer.predict(roi_grey)
+            if conf>=65 and conf <= 90:
+                print(id_)
+                print(labels[id_])
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                color = (255, 255, 255)
+                stroke = 2
+                name = labels[id_]
+                cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+            img_item = "my-image-clr.png"
+            cv2.imwrite(img_item, roi_color)
+
+            color = (255, 0, 0)
             stroke = 2
-            name = labels[id_]
-            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+            end_coord_x = x + w
+            end_coord_y = y + h
+            cv2.rectangle(frame, (x,y), (end_coord_x, end_coord_y), color, stroke)
 
-        img_item = "my-image-clr.png"
-        cv2.imwrite(img_item, roi_color)
+        #display resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
 
-        color = (255, 0, 0)
-        stroke = 2
-        end_coord_x = x + w
-        end_coord_y = y + h
-        cv2.rectangle(frame, (x,y), (end_coord_x, end_coord_y), color, stroke)
+    cap.release()
+    cv2.destroyAllWindows()
 
-    #display resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+main()
