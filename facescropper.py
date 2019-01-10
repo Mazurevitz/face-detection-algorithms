136ec4dbf9426f456e004c11e40d9d393c92d579
Wojciech Mazur <wojtekmaz.glowny@gmail.com>
2019-01-10 03:33:21 +0100

    Dirty final commit

--- facescropper.py
+++ facescropper.py
@@ -3,8 +3,101 @@ from pathlib import Path
 import math
 from operator import itemgetter
 from matplotlib import pyplot as plt
-import cv2
 import numpy as np
+from sklearn.cluster import KMeans
+from mpl_toolkits.mplot3d import Axes3D
+import cv2
+
+class DominantColors:
+
+    CLUSTERS = None
+    IMAGE = None
+    COLORS = None
+    LABELS = None
+    
+    def __init__(self, image, clusters=3):
+        self.CLUSTERS = clusters
+        self.IMAGE = image
+        
+    def dominantColors(self):
+    
+        #read image
+        # img = cv2.imread(self.IMAGE)
+        img = self.IMAGE
+        
+        #convert to rgb from bgr
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                
+        #reshaping to a list of pixels
+        img = img.reshape((img.shape[0] * img.shape[1], 3))
+        
+        #save image after operations
+        self.IMAGE = img
+        
+        #using k-means to cluster pixels
+        kmeans = KMeans(n_clusters = self.CLUSTERS)
+        kmeans.fit(img)
+        
+        #the cluster centers are our dominant colors.
+        self.COLORS = kmeans.cluster_centers_
+        
+        #save labels
+        self.LABELS = kmeans.labels_
+        
+        #returning after converting to integer from float
+        return self.COLORS.astype(int)
+
+    def rgb_to_hex(self, rgb):
+        return '#%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
+    
+    def plotClusters(self):
+        #plotting 
+        fig = plt.figure()
+        ax = Axes3D(fig)        
+        for label, pix in zip(self.LABELS, self.IMAGE):
+            ax.scatter(pix[0], pix[1], pix[2], color = self.rgb_to_hex(self.COLORS[label]))
+        plt.show()
+
+    def plotHistogram(self):
+       
+        #labels form 0 to no. of clusters
+        numLabels = np.arange(0, self.CLUSTERS+1)
+       
+        #create frequency count tables    
+        (hist, _) = np.histogram(self.LABELS, bins = numLabels)
+        hist = hist.astype("float")
+        hist /= hist.sum()
+        
+        #appending frequencies to cluster centers
+        colors = self.COLORS
+        
+        #descending order sorting as per frequency count
+        colors = colors[(-hist).argsort()]
+        hist = hist[(-hist).argsort()] 
+        
+        #creating empty chart
+        chart = np.zeros((50, 500, 3), np.uint8)
+        start = 0
+        
+        #creating color rectangles
+        for i in range(self.CLUSTERS):
+            end = start + hist[i] * 500
+            
+            #getting rgb values
+            r = colors[i][0]
+            g = colors[i][1]
+            b = colors[i][2]
+            
+            #using cv2.rectangle to plot colors
+            cv2.rectangle(chart, (int(start), 0), (int(end), 50), (r,g,b), -1)
+            start = end	
+        
+        #display chart
+        plt.figure()
+        plt.axis("off")
+        plt.imshow(chart)
+        # plt.show()
+
 
 eye_cascade = cv2.CascadeClassifier('haar/haarcascade_eye.xml')
 eyes_number = 2
@@ -20,11 +113,11 @@ def rotation_matrix_from_eyes(face_image, w, h):
     eye_ctr = 0
     for (ex,ey,ew,eh) in eyes:
         eye_ctr += 1
-        # print('eye/w = ',ew/w)
-        # if(ew/w < 0.5):
-        #     cv2.rectangle(face_image,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        #     cv2.circle(face_image, (math.floor(ex+ew/2),math.floor(ey+eh/2)),(5),(0,0,255),2)
-        # print('eye_ctr: ', eye_ctr)
+        print('eye/w = ',ew/w)
+        if(ew/w < 0.5):
+            cv2.rectangle(face_image,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.circle(face_image, (math.floor(ex+ew/2),math.floor(ey+eh/2)),(5),(0,0,255),2)
+        print('eye_ctr: ', eye_ctr)
 
     if len(eyes) > 1:
         if (rotation < 0):
@@ -60,24 +153,27 @@ def select_and_crop_eyes(face_image, crop_by_number):
 
 def detect_color(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    
+
+    # lower = np.array([0, 48, 80])
+    # upper = np.array([20, 255, 255])
+
     lower = np.array([0, 48, 80])
     upper = np.array([20, 255, 255])
-    
+
     converted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     skinMask = cv2.inRange(converted, lower, upper)
- 
+
 	# apply a series of erosions and dilations to the mask
 	# using an elliptical kernel
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
     skinMask = cv2.erode(skinMask, kernel, iterations = 2)
     skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
- 
+
 	# blur the mask to help remove noise, then apply the
 	# mask to the frame
     skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
     skin = cv2.bitwise_and(image, image, mask = skinMask)
- 
+
 	# show the skin in the image along with the mask
 
     cv2.imshow("skin", skin)
@@ -95,6 +191,24 @@ def detect_color(image):
     # cv2.imshow('mask',mask)
     # cv2.imshow('res',res)
 
+def cluster_color_show(img):
+    # img = cv2.imread(img_name)
+    Z = img.reshape((-1,3))
+
+    # convert to np.float32
+    Z = np.float32(Z)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 12
+    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+
+    cv2.imshow('res2',res2)
 
 def main():
     face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
@@ -103,6 +217,7 @@ def main():
     image_dir = os.path.join(base_dir, "images")
 
     progress = 0
+    clusters = 6
 
     for root, dirs, files in os.walk(image_dir):
         for file in files:
@@ -113,9 +228,11 @@ def main():
 
                 IMG = cv2.imread(path)
                 cv2.imshow('before cutout', IMG)
+                
+                green = np.uint8([[[0,255,0 ]]])
+                hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
+                print (hsv_green)
 
-                # IMG = detect_color(IMG)
-                cv2.imshow('after cutout', IMG)
                 # print("cutout show!")
 
                 IMG_copy = cv2.imread(path)
@@ -132,6 +249,33 @@ def main():
 
                     roi_gray = gray[y:y+h, x:x+w]
                     roi_color = IMG[y:y+h, x:x+w]
+
+                    cluster_color_show(roi_color)
+
+                    dc = DominantColors(roi_color, clusters)
+                    colors = dc.dominantColors()
+                    print('rgb', colors)
+                    print(colors[0])
+
+                    color_array = np.zeros((1,clusters,3), dtype=int)
+                    color_array[0] = colors
+                    print('color array: ', color_array)
+
+                    hsv_colors = cv2.cvtColor(np.uint8(color_array), cv2.COLOR_RGB2HSV)
+                    print('hsv', hsv_colors)
+
+                    # TEST TEST TEST
+                    color_test = np.zeros((1,1,3), dtype=int)
+                    color_test[0][0] = colors[0]
+                    print('color test: ', color_test)
+
+                    hsv_test = cv2.cvtColor(np.uint8(color_test), cv2.COLOR_RGB2HSV)
+                    print('hsv test', hsv_test)
+                    # END OF TEST
+
+                    # face_cutout = detect_color(roi_color)
+                    # cv2.imshow('after cutout', face_cutout)
+
 
                     roi_gray = cv2.resize(roi_gray, (250,250))
                     roi_color = cv2.resize(roi_color, (250,250))
@@ -177,15 +321,21 @@ def main():
                 print("progress: {0}/{1}".format(progress, 50))
                 progress += 1
 
-                cv2.imshow('1.original', IMG_copy)
-                cv2.imshow('2.face that was found', roi_color)
-                cv2.imshow('3.face in grey', roi_gray)
-                cv2.imshow('4.after rotation', roi_gray_warped)
-                cv2.imshow('5.warped original', img_warped)
-                cv2.imshow('6.after rotation', new_roi_gray_warped)
-                cv2.imshow('7.after normalization', eq_histogram_image)
-                plt.show()
+                # cv2.imshow('1.original', IMG_copy)
+                # cv2.imshow('2.face that was found', roi_color)
+                # cv2.imshow('3.face in grey', roi_gray)
+                # cv2.imshow('4.after rotation', roi_gray_warped)
+                # cv2.imshow('5.warped original', img_warped)
+                # cv2.imshow('6.after rotation', new_roi_gray_warped)
+                # cv2.imshow('7.after normalization', eq_histogram_image)
+                # plt.show()
 
                 cv2.waitKey(0)
 
+# img = 'paul.png'
+# clusters = 6
+# dc = DominantColors(img, clusters)
+# colors = dc.dominantColors()
+# print(colors)
 main()
+# cluster_color_show(img)
--- facesrec-video.py
+++ facesrec-video.py
@@ -2,43 +2,133 @@ import numpy as np
 import cv2
 import pickle
 from math import log1p
-import os.path 
+import os.path
+from matplotlib import pyplot as plt
+import matplotlib.patches as mpatches
+import time
+
+default_color = (255, 255, 255)
+correctly_recognized_color = (0, 255, 0)
+not_recognized_color = (0, 0, 255)
+
+def make_upper_lower(value, frame_size, error):
+    return value*1-(frame_size*error), value*1+(frame_size*error)
+
+# def face_in_boundary(current_frame, starting_frame, ending_frame,
+#                x, y, face_x, face_y, vid_w, vid_h):
+#     face_x_down, face_x_up = make_upper_lower(face_x, vid_w, 0.1)
+#     face_y_down, face_y_up = make_upper_lower(face_y, vid_h, 0.1)
+
+#     if (current_frame > starting_frame and current_frame < ending_frame
+#         and x > face_x_up and y > face_y_up
+#         and x < face_x_down and y < face_y_down):
+#         return True
+
+
+def get_recognition_color(detected_name, actual_name, current_frame, starting_frame, ending_frame,
+               x, y, face_x, face_y, vid_w, vid_h):
+    face_x_down, face_x_up = make_upper_lower(face_x, vid_w, 0.1)
+    face_y_down, face_y_up = make_upper_lower(face_y, vid_h, 0.1)
+
+    color = not_recognized_color
+
+    if (current_frame > starting_frame and current_frame < ending_frame
+            and x < face_x_up and y < face_y_up
+            and x > face_x_down and y > face_y_down):
+        if detected_name == actual_name:
+            # print("correctly recognized {0}".format(detected_name))
+            color = correctly_recognized_color
+        else:
+            # print("not recognized correctly")
+            color = not_recognized_color
+    else:
+        color = default_color
+
+    return color
+
+# def assign_face_check():
+#     if(frame > 0 and frame < 238):
+#         color = get_recognition_color(name, "bryan-cranston", current_frame, 0, 238, x, y, 290, 70, width, height)
+
+#     # if(frame > 239 and frame < 342):
+#     #     color = get_recognition_color(name, "eddie-redmaine", current_frame, 239, 342, x, y, 290, 80, width, height)
+#     #     color = get_recognition_color(name, "benedict-cumberbatch", current_frame, 239, 342, x, y, 80, 40, width, height)
+#     #     color = get_recognition_color(name, "bryan-cranston", current_frame, 239, 342, x, y, 515, 60, width, height)
+
+#     if(frame > 238 and frame < 416):
+#         color = get_recognition_color(name, "eddie-redmaine", current_frame, 343, 520, x, y, 240, 70, width, height)
+
+#     if(frame > 417 and frame < 651):
+#         color = get_recognition_color(name, "benedict-cumberbatch", current_frame, 521, 755, x, y, 260, 75, width, height)
 
 def main():
-    video_name = "redmaine-eddie-cumber"
+    video_name = "redmaine-eddie-cumber-short-cut"
     cap = cv2.VideoCapture("videos/{0}.mp4".format(video_name))
 
-    face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
-    eye_cascade = cv2.CascadeClassifier('haar/haarcascade_eye.xml')
+    current_frame = 0
+    property_id = int(cv2.CAP_PROP_FRAME_COUNT)
+    width = cap.get(3)  # float
+    height = cap.get(4) # float
 
-    # recognizer = cv2.face.EigenFaceRecognizer_create()
-    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    video_length_frames = int(cv2.VideoCapture.get(cap, property_id))
+    print("video length: {0}".format(video_length_frames))
+
+    face_cascade = cv2.CascadeClassifier(
+        'haar/haarcascade_frontalface_alt2.xml')
+    # eye_cascade = cv2.CascadeClassifier('haar/haarcascade_eye.xml')
+
+    recognizer = cv2.face.EigenFaceRecognizer_create()
+    # recognizer = cv2.face.LBPHFaceRecognizer_create()
     recognizer.read("trainer.yml")
 
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    
+
     video_name = "_".join([video_name, 'output2.avi'])
-    output_destination = os.path.join("output-video", video_name)
-    out = cv2.VideoWriter(output_destination, fourcc, 20.0, (640, 360))
+    output_destination = os.path.join("output-videos", video_name)
+    out = cv2.VideoWriter(output_destination, fourcc, 20.0, (int(width), int(height)))
 
     labels = {}
+    font = cv2.FONT_HERSHEY_DUPLEX
+    stroke = 1
+
+    recognition_frame = [0 for x in range(video_length_frames+1)]
+    recognition_values = [0 for x in range(video_length_frames+1)]
+
+    plt.figure(figsize=(15,5), dpi=120)
+    plt.xlim(0, video_length_frames)
+    plt.ylim(10000, 15000)
+
+    badly_recognized = 0
+    not_a_face = 0
+
+    start = time.time()
+
     with open("labels.pickle", 'rb') as f:
         og_labels = pickle.load(f)
-        labels = {v:k for k,v in og_labels.items()}
+        labels = {v: k for k, v in og_labels.items()}
 
     while(True):
-        #capture frame by frame
+        # capture frame by frame
         ret, frame = cap.read()
         if not ret:
             break
 
+        print("frame: {0}/{1}".format(current_frame, video_length_frames))
+        current_frame += 1
+        current_frame_text = "current frame: {0}/{1}".format(current_frame, video_length_frames)
+        cv2.putText(frame, current_frame_text, (20, 340), font,
+                    0.5, default_color, stroke, cv2.LINE_AA)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+        faces = face_cascade.detectMultiScale(
+            gray, scaleFactor=1.1, minNeighbors=5)
+        
+        conf = 0
+        recognition_values[current_frame] = conf
 
         for (x, y, w, h) in faces:
-            # print(x,y,w,h)
+            # print(x, y, w, h)
             roi_gray = gray[y:y+h, x:x+w]
-            roi_gray = cv2.resize(roi_gray, (250,250))
+            roi_gray = cv2.resize(roi_gray, (250, 250))
             # print(len(roi_gray))
             roi_color = frame[y:y+h, x:x+w]
 
@@ -49,35 +139,78 @@ def main():
             id_, conf = recognizer.predict(roi_gray)
             # print("id: {0}, conf: {1}".format(id_, conf))
             # if conf>=12000:
-                # print(id_)
-                # print(labels[id_])
-            font = cv2.FONT_HERSHEY_DUPLEX
-            color = (255, 255, 255)
-            stroke = 1
+            # print(id_)
+            # print(labels[id_])
             name = labels[id_]
-            cv2.putText(frame, name, (x,y), font, log1p(w/64), color, stroke, cv2.LINE_AA)
+
+            if(current_frame > 0 and current_frame < 238):
+                color = get_recognition_color(name, "bryan-cranston", current_frame, 0, 238, x, y, 290, 70, width, height)
+
+            elif(current_frame > 238 and current_frame < 416):
+                color = get_recognition_color(name, "eddie-redmaine", current_frame, 238, 416, x, y, 240, 70, width, height)
+
+            elif(current_frame > 417 and current_frame < 651):
+                color = get_recognition_color(name, "benedict-cumberbatch", current_frame, 417, 651, x, y, 260, 75, width, height)
+
+
+
+
+            name_confidence = "{0} conf:{1}".format(name, int(conf))
+            coordinates = "x:{0} y:{1} w:{2} h:{3}".format(x, y, w, h)
+            cv2.putText(frame, name_confidence, (x-int(w), y),
+                        font, log1p(w/128), color, stroke, cv2.LINE_AA)
+            cv2.putText(frame, coordinates, (x-int(w/2), y+int(h*1.2)),
+                        font, log1p(w/128), color, stroke, cv2.LINE_AA)
+
             # print(name)
 
             # img_item = "my-image-clr.png"
             # cv2.imwrite(img_item, roi_color)
 
-            color = (255, 0, 0)
-            stroke = 2
+            # color = (255, 0, 0)
+            # stroke = 1
             end_coord_x = x + w
             end_coord_y = y + h
-            cv2.rectangle(frame, (x,y), (end_coord_x, end_coord_y), color, stroke)
+            cv2.rectangle(frame, (x, y), (end_coord_x,
+                                          end_coord_y), color, stroke)
+
+        recognition_frame[current_frame] = current_frame
+        recognition_values[current_frame] = conf
+        if color == not_recognized_color:
+            badly_recognized += 1
+            plt.axvspan(current_frame, current_frame+1, facecolor='red', alpha=0.3, label="badly recognized")
+
+        elif color == default_color:
+            not_a_face += 1
+            plt.axvspan(current_frame, current_frame+1, facecolor='darkorange', alpha=0.3, label="not a face")
+        elif conf == 0:
+            plt.axvspan(current_frame, current_frame+1, facecolor='black', alpha=0.3, label="no face detected")
 
 
-        #display resulting frame
+
+        # display resulting frame
         # cv2.imshow('frame', frame)
         out.write(frame)
         if cv2.waitKey(20) & 0xFF == ord('q'):
             cap.release()
             out.release()
             cv2.destroyAllWindows()
-
             break
+    
+    end = time.time()
+    print(end - start)
+    print("wrongly recognized: {0}%, not a face: {1}%, correct: {2}%"
+          .format(round(badly_recognized/video_length_frames, 3),
+                  round(not_a_face/video_length_frames, 3),
+                  round((video_length_frames-not_a_face-badly_recognized)/video_length_frames, 3)))
 
+    plt.plot(recognition_frame, recognition_values, label="confidence")
+    red_patch = mpatches.Patch(color='red', alpha=0.3, label='Wrong recognition')
+    orange_patch = mpatches.Patch(color='darkorange', alpha=0.3, label='Not a face')
+    black_patch = mpatches.Patch(color='black', alpha=0.3, label='No face detected')
+    plt.legend(handles=[red_patch, orange_patch, black_patch])
+    plt.savefig('performance_{0}.{1}'.format(video_name, 'jpg'), bbox_inches='tight')
+    plt.show()
     cap.release()
     out.release()
     cv2.destroyAllWindows()
